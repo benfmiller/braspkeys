23c3cbc42e0d2dd619c8f84e9dde79ec836286a3
Ben Miller <ben.f.miller24@gmail.com>
2020-12-04 14:59:12 -0700

    made main file

--- __main__.py
+++ __main__.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python3
 
+import json
 import time
+import sys
+
+with open("key_code_dict.json", "r") as file:
+    key_codes = json.load(file)
 
 NULL_CHAR = chr(0)
 CTRL_KEY = chr(1)
