1c0d02211e0aa8131197e8e16e6238f3f7d6aa58
Ben Miller <ben.f.miller24@gmail.com>
2020-12-04 15:30:03 -0700

    added input and write function

--- __main__.py
+++ __main__.py
@@ -1,16 +1,18 @@
 #!/usr/bin/env python3
-
+import keyboard.keyboard as keyboard
 import json
 import time
 import sys
+import fileinput
 
-with open("key_code_dict.json", "r") as file:
-    key_codes = json.load(file)
-
-NULL_CHAR = chr(0)
-CTRL_KEY = chr(1)
+# with open("key_code_dict.json", "r") as file:
+#     key_codes = json.load(file)
 
 
-def write_report(report):
-    with open("/dev/hidg0", "rb+") as fd:
-        fd.write(report.encode())
+# def write_report(report):
+#     with open("/dev/hidg0", "rb+") as fd:
+#         fd.write(report.encode())
+
+for line in fileinput.input():
+    # print(line, end="")
+    keyboard.write(line)
